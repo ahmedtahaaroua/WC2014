3a44b2c7c8555ad07da2f5cf5dee8ee3353ec893
Oukhay <oukhayhamed@gmail.com>
2014-06-04 19:11:32 +0200

    TeamController Modification

--- src/tn/fallayaTeam/worldcupapp2014/controller/TeamController.java
+++ src/tn/fallayaTeam/worldcupapp2014/controller/TeamController.java
@@ -96,4 +96,29 @@ public class TeamController {
 		}
 		return to_return;
 	}
+	static public List<Team> getTeam( String result) {
+		List<Team> to_return=new ArrayList<Team>();
+
+		result="{\"Android\":"+result+"}";
+		System.out.println(result+"eee");
+		try {
+			JSONObject jsonObject=new JSONObject(result);
+			JSONArray jsonArray = jsonObject.optJSONArray("Android");
+			System.out.println("sqsss"+jsonArray.length());
+			for(int i=0;i<jsonArray.length();i++){
+			Team Team=new Team();
+			Team.setId(Integer.parseInt(jsonArray.getJSONObject(i).get("iId").toString()));
+			Team.setName(jsonArray.getJSONObject(i).get("sName").toString());
+			Team.setUrlMinFlag(jsonArray.getJSONObject(i).get("sCountryFlag").toString());
+			Team.setUrlMaxFlag(jsonArray.getJSONObject(i).get("sCountryFlagLarge").toString());	
+			Team.setUrlWiki(jsonArray.getJSONObject(i).get("sWikipediaURL").toString());
+				to_return.add(Team);
+			}
+			
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return to_return;
+	}
 }
