34ebb035263becb9169b9a5eedffc31d5b0c74ff
Oukhay <oukhayhamed@gmail.com>
2014-06-04 19:31:53 +0200

    GroupInfo Entity

---
+++ src/tn/fallayaTeam/worldcupapp2014/entities/GroupInfo.java
@@ -0,0 +1,21 @@
+package tn.fallayaTeam.worldcupapp2014.entities;
+
+public class GroupInfo {
+	String code;
+	String description;
+	
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+
+}
